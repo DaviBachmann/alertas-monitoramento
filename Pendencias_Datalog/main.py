a0425dbabd98dbb9b8c43f1f1148692e363a7342
DaviBachmann <davibachmann0904@gmail.com>
2025-07-28 14:41:00 -0300

    Add files via upload

---
+++ Pendencias_Datalog/main.py
@@ -0,0 +1,72 @@
+# encoding: utf-8
+
+# Importa as bibliotecas necessárias
+import os
+import schedule
+import time
+
+from features.checker import verificar_tarefas_datalog
+from core.db_utils import conectar_banco, fechar_conexao
+from core.utils import carregar_arquivo_env
+
+def main():
+    
+    """
+    Função principal: carrega variáveis e executa verificação das tarefas.
+    """
+    
+    # Carrega as variáveis de ambiente do arquivo .env
+    try:
+        carregar_arquivo_env()
+        
+    except Exception as e:
+        print(f"Erro ao carregar variáveis de ambiente: {e}")
+
+    db_server = os.getenv("db_server") # Servidor do banco de dados
+    db_user = os.getenv("db_user") # Usuário do banco de dados
+    db_password = os.getenv("db_password") # Senha do usuário
+    db_name = os.getenv("db_name") # Nome do banco de dados
+    
+    try:
+        # Conecta ao banco de dados
+        conn = conectar_banco(db_server, db_name, db_user, db_password)
+    except Exception as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+
+    # Informações para o envio de e-mail
+    smtp_info = {
+        "smtp_server": os.getenv("smtp_server"),
+        "smtp_port": os.getenv("smtp_port"),
+        "email_from": os.getenv("email_from"),
+        "email_password": os.getenv("email_password"),
+    }
+    destinatario = os.getenv("EMAIL_TO")
+
+    # Executa a verificação
+    try:
+        verificar_tarefas_datalog(
+            conn=conn, 
+            smtp_info=smtp_info, 
+            destinatario=destinatario
+        )
+    except Exception as e:
+        print(f'Erro ao verificar tarefas: {e}')
+    
+    try:            
+        fechar_conexao(conn)
+    except Exception as e:
+        print(f"Erro ao fechar a conexão com o banco de dados: {e}")
+
+# Agendamento da rotina
+horarios = [f"{hora:02d}:00" for hora in range(8, 18)]
+dias_uteis = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+
+for dia in dias_uteis:
+    for horario in horarios:
+        getattr(schedule.every(), dia).at(horario).do(main)
+
+# Loop de execução
+if __name__ == "__main__":
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
